bdfb4740e36cdad0864aa088c807b57b3e3875d4
Gabriel Monteiro Fernandes <mfgbriel92@gmail.com>
2019-06-24 16:37:33 -0300

    Módulo 01 - CRUD

--- modulo01/index.js
+++ modulo01/index.js
@@ -1,14 +1,43 @@
 const express = require('express')
 const server = express()
 
+server.use(express.json())
+
+const users = ['Gabriel', 'Jeryanne', 'Sarita']
+
+server.get('/users', (req, res) => {
+  return res.json({ users })
+})
+
 server.get('/users/:id', (req, res) => {
-  const { name } = req.query
   const { id } = req.params
 
-  return res.json({
-    message: `Hello, ${name}`,
-    id
-  })
+  return res.json(users[id])
+})
+
+server.post('/users', (req, res) => {
+  const { name } = req.body
+
+  users.push(name)
+
+  return res.json({ users })
+})
+
+server.put('/users/:id', (req, res) => {
+  const { id } = req.params
+  const { name } = req.body
+
+  users[id] = name
+
+  return res.json({ users })
+})
+
+server.delete('/users/:id', (req, res) => {
+  const { id } = req.params
+
+  users.splice(id, 1)
+
+  return res.send()
 })
 
 server.listen(3333)
